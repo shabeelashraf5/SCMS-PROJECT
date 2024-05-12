d6991c4623daaef2f37d3fca9ce3b2dec28df3dc
shabeelashraf5 <05shebz@gmail.com>
2024-05-12 18:01:48 +0530

    Updated files

--- server/auth/sentEmail.js
+++ server/auth/sentEmail.js
@@ -24,8 +24,8 @@ const sentMail = async function (fname,email,token ) {
         from: process.env.SMTP_USER,
         to: email,
         subject: 'Welcome to the Platform ! Reset your Password',
-       // html: `<h2> Hello ${fname}, Welcome to our Team. Please click to <a href="${process.env.URLs}/reset-password/?token=${token}">Reset</a> your password<h2>`
-        html: `<h2> Hello ${fname}, Welcome to our Team. Please click to <a href="http://localhost:4200/reset-password/?token=${token}">Reset</a> your password<h2>`
+        html: `<h2> Hello ${fname}, Welcome to our Team. Please click to <a href="${process.env.URLs}/reset-password/?token=${token}">Reset</a> your password<h2>`
+        //html: `<h2> Hello ${fname}, Welcome to our Team. Please click to <a href="http://localhost:4200/reset-password/?token=${token}">Reset</a> your password<h2>`
     };
 
    
